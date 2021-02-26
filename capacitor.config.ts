486fb701783a92b0c8ddc1aecd0cd9e9ac69a270
Valeriy Garbuzov <valeriy@i.ua>
2021-02-26 19:56:21 +0200

    init base angular capacitor-3 project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'cap3-camera-ex',
+  webDir: 'dist/cap3-camera-ex',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cap3-camera-ex';
+  image: any
+
+  async takePhoto() {
+    alert('click on takePhoto()')
+  }
 }
